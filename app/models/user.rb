0a5baae9b4d060b3ee16381ffb030e2ee388b850
fionasheh“git config --global user.email fionasheh@163.com“ <fionasheh@zhanggendeMacBook-Air.local>
2017-04-04 14:26:39 +0800

    join or quit group

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  has_many :groups
   has_many :posts
   has_many :group_relationships
   has_many :participated_groups, :through => :group_relationships, :source => :group
@@ -10,4 +12,13 @@ class User < ApplicationRecord
    def is_member_of?(group)
      participated_groups.include?(group)
    end
+
+   def join!(group)
+     participated_groups << group
+   end
+
+   def quit!(group)
+     participated_groups.delete(group)
+   end
+   
 end
